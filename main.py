caa41be632f7f122e324faf022cad9de294ad5d7
Marek Pelowski <marek.pelowski@protonmail.com>
2023-05-05 16:00:40 +0200

    created the background

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from tkinter import *
 
+# Colours used for the background:
+# #5188b5
+# #79aed9
+# 4b81ad
+# #5e9dd1
+# #052f52
+
 root = Tk()
-root.geometry("750x500")
+root.geometry("1000x600")
 root.title("Rock Paper Scissors")
 root.resizable(0, 0)
+root.configure(bg="#4b81ad")
+
+choosing_panel_frame = Frame(root, bg="#79aed9", height=600, width=240)
+choosing_panel_frame.place(x=760, y=0)
+
 
 
 
