15a402b4640530679eac5c11d1d44d617f84a087
Marek Pelowski <marek.pelowski@protonmail.com>
2023-05-07 14:55:07 +0200

    added rock button

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ from tkinter import *
 # #5e9dd1
 # #052f52
 
-rock = PhotoImage(file=r"paper.png")
-paper = PhotoImage(file=r"paper.png")
-scissors = PhotoImage(file=r"paper.png")
+
 
 root = Tk()
 root.geometry("1000x600")
@@ -17,11 +15,17 @@ root.title("Rock Paper Scissors")
 root.resizable(0, 0)
 root.configure(bg="#4b81ad")
 
+rock_image = PhotoImage(file=r"rock.png")
+paper_image = PhotoImage(file=r"paper.png")
+scissors_image = PhotoImage(file=r"scissors.png")
+
+
 selecting_frame = Frame(root, bg="#79aed9", height=600, width=240)
 selecting_frame.place(x=760, y=0)
 
 
-
+rock_button = Button(root, image=rock_image, borderwidth=0, bg="#79aed9")
+rock_button.place(x=800, y=15)
 
 
 
