7a9fad746b1850ee9564f19343f33f36e9a7205f
Marek Pelowski <marek.pelowski@protonmail.com>
2023-05-22 21:38:00 +0200

    added the enemys avatar to the game

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ rock_image = PhotoImage(file=r"rock.png")
 paper_image = PhotoImage(file=r"paper.png")
 scissors_image = PhotoImage(file=r"scissors.png")
 player_image = PhotoImage(file=r"finger_pointing_at_you.png")
+enemy_image = PhotoImage(file=r"robot.jpg")
 
 
 selecting_frame = Frame(root, bg="#79aed9", height=600, width=240)
@@ -33,13 +34,16 @@ enemy_frame.place(x=525, y=100)
 text_frame = Frame(root, bg="#79aed9", height=60, width=760)
 text_frame.place(x=0, y=0)
 
-
 text_frame_text = Label(root, text="Rock Paper Scissors Game", bg="#79aed9", fg="black", font=("consolas", 22, "bold"))
 text_frame_text.place(x=200, y=10)
 
 
+
 player_avatar = Label(root, image=player_image, borderwidth=0, bg="#3f6a99")
 player_avatar.place(x=34, y=110)
+
+enemy_avatar = Label(root, image=enemy_image, borderwidth=0, bg="#3f6a99")
+enemy_avatar.place(x=200, y=110)
 
 
 rock_button = Button(root, image=rock_image, borderwidth=0, bg="#79aed9")
